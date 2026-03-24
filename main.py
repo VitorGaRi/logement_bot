1ee848f50dc1c19115c71734b802d05f55876769
VitorGaRi <81936481+VitorGaRi@users.noreply.github.com>
2026-03-24 11:31:56 +0100

    Refactor monitoring logic and add loop for checks

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 import requests
 import os
+import time
 
 URL = "https://www.studefi.fr/main.php?srv=Residence&op=show&cdGroupe=801G"
-TEXTO = "Aucun logement disponibles"
+TEXTO = "Aucun logement disponible"
 
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-print("TOKEN:", TOKEN)
-print("CHAT_ID:", CHAT_ID)
-
 def enviar(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    response = requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
-    
-    print("Status code:", response.status_code)
-    print("Resposta:", response.text)
+    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
 
 def check():
     headers = {"User-Agent": "Mozilla/5.0"}
     r = requests.get(URL, headers=headers, timeout=10)
     return TEXTO in r.text
 
-print("Verificando disponibilidade...")
+print("Iniciando monitoramento...")
 
 try:
-    indisponivel = check()
+    for i in range(29):  # 29 vezes = quase 5 min se usar 10s
+        print(f"Check {i+1}/30")
 
-    if not indisponivel:
-        print("🚨 LOGEMENT DISPONÍVEL!!!")
-        enviar("🚨 LOGEMENT DISPONÍVEL!!!")
+        indisponivel = check()
 
-    else:
-        print("Nada ainda...")
+        if not indisponivel:
+            print("🚨 LOGEMENT DISPONÍVEL!!!")
+            enviar("🚨 LOGEMENT DISPONÍVEL!!!")
+            break  # para imediatamente se achar vaga
+
+        time.sleep(10)
+
+    print("Loop finalizado")
 
 except Exception as e:
     print("Erro:", e)
